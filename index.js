ecc5afd928c04d3adbb20449db237db612a1ae7b
Arpitadey88 <deyarpitabanik@gmail.com>
2021-11-13 12:32:51 +0600

    added new

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const { MongoClient } = require('mongodb');
 const ObjectId = require('mongodb').ObjectId;
 
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
